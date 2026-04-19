e7c8ca7670cc5467bcd0abf2afd1e16d84848f10
Chibuzor <chibuzormekalam@gmail.com>
2026-04-19 10:12:35 +0100

    fixng landing page4

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -12,35 +12,31 @@ export default function LandingPage () {
     <div className='bg-[#050B0B] min-h-screen font-sans '>
       <div className='w-full'>
         <div className='w-full flex justify-center items-center'>
-          <div className=' w-full md:w-[70%]'>
-            <div className='w-full flex flex-col justify-center '>
-              <div className='header flex justify-between items-center px-4 py-3'>
-                <div className='flex items-center gap-3'>
-                  <span
-                    className='bg-center bg-cover h-[20px] lg:h-[40px] w-[20px] lg:w-[40px] block'
-                    style={{
-                      backgroundImage: `url("/images/zeflogo.png") `
-                    }}
-                  ></span>
+          <div className='header flex justify-between items-center px-4 py-3 w-full'>
+            <div className='flex items-center gap-3'>
+              <span
+                className='bg-center bg-cover h-[20px] lg:h-[40px] w-[20px] lg:w-[40px] block'
+                style={{
+                  backgroundImage: `url("/images/zeflogo.png") `
+                }}
+              ></span>
 
-                  <p className=' font-semibold text-white'>Zephyr</p>
-                </div>
+              <p className=' font-semibold text-white'>Zephyr</p>
+            </div>
 
-                <div className='flex items-center gap-6 text-xs lg:text-[16px]'>
-                  <Link
-                    to={'https://zephyr-web-sable.vercel.app/how_it_works'}
-                    className='font-[400] text-[#FFFFFFB3]'
-                  >
-                    How it works
-                  </Link>
-                  <Link
-                    to={'https://zephyr-web-sable.vercel.app'}
-                    className='bg-[#006557] text-white  font-[700] px-2 md:px-4 py-2.5 rounded-lg hover:bg-white/90 transition-colors shadow-[0px_0px_28px_rgba(0,101,87,1)]'
-                  >
-                    Launch App
-                  </Link>
-                </div>
-              </div>
+            <div className='flex items-center gap-6 text-xs lg:text-[16px]'>
+              <Link
+                to={'https://zephyr-web-sable.vercel.app/how_it_works'}
+                className='font-[400] text-[#FFFFFFB3]'
+              >
+                How it works
+              </Link>
+              <Link
+                to={'https://zephyr-web-sable.vercel.app'}
+                className='bg-[#006557] text-white  font-[700] px-2 md:px-4 py-2.5 rounded-lg hover:bg-white/90 transition-colors shadow-[0px_0px_28px_rgba(0,101,87,1)]'
+              >
+                Launch App
+              </Link>
             </div>
           </div>
         </div>
